3ee547ea519df7aa8fb904724515172facc4c6df
Sasha Chementsova <101978806+SashaChementsova@users.noreply.github.com>
2022-03-28 00:07:46 +0300

    Добавьте файлы проекта.

---
+++ Server/main_server.cpp
@@ -0,0 +1,315 @@
+#define _AFXDLL  //содержат MFC в DLL
+#include <iostream>
+#include <afx.h>  //для ускорения компиляции проектов
+#include <winsock2.h>
+#include <process.h> /* _beginthread, _endthread */  //содержит объявления функций и макросы, используемые при работе с потоками и процессами
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+void main_working(void* newS) {
+	int c, c1 = 0, c2 = 0, c3;
+	int flag = 0;
+	char p[500], com[200], k[500], m[500];
+	com[0] = '\0'; p[0] = '\0'; k[0] = '\0'; m[0] = '\0';
+	cout << "Сервер работает.\n";
+	while (1) {
+		strcpy_s(p, "Добро пожаловать в ПАО ИнвестЛаб.\n");
+		send((SOCKET)newS, p, sizeof(p), 0);
+		strcpy_s(k, "Меню:\n 1)Войти в качестве администратора;\n 2)Войти в качестве клиента;\n 3)Войти в качестве представителя инвестиционного объекта;\n 4)Войти в качестве эксперта;\n 5)Завершить работу программы.\n ");
+		send((SOCKET)newS, k, sizeof(k), 0);
+		p[0] = '\0'; k[0] = '\0';
+		recv((SOCKET)newS, m, sizeof(m), 0);
+		c = atoi(m);
+		switch (c) {
+		case 1: {
+			strcpy_s(p, "1");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			strcpy_s(p, "*ЗДЕСЬ БУДЕТ АВТОРИЗАЦИЯ*");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			while (c1 != 5) {
+				strcpy_s(k, "Меню администратора:\n 1)Управление клиентами;\n 2)Принятие решения;\n 3)Управление экспертами;\n 4)Управление инвестиционными объектами;\n 5)Выйти в главное меню.\n ");
+				send((SOCKET)newS, k, sizeof(k), 0);
+				recv((SOCKET)newS, m, sizeof(m), 0);
+				c1 = atoi(m);
+				switch (c1) {
+				case 1: {
+					strcpy_s(p, "1");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					while (c2 != 6) {
+						strcpy_s(p, "Меню управления клиентами:\n 1)Вывод таблицы со всеми клиентами в алфавитном порядке;\n 2)Разрыв договора с клиентом;\n 3)Редактирование данных клиента;\n 4)Вывод договора клиента;\n 5)Фильтрация;\n 6)Выйти в меню администратора.\n");
+						send((SOCKET)newS, p, sizeof(p), 0);
+						recv((SOCKET)newS, m, sizeof(m), 0);
+						c2 = atoi(m);
+						switch (c2) {
+						case 1: {
+							strcpy_s(p, "1");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 2: {
+							strcpy_s(p, "2");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 3: {
+							strcpy_s(p, "3");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 4: {
+							strcpy_s(p, "4");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 5: {
+							strcpy_s(p, "5");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 6: {
+							strcpy_s(p, "6");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						}
+					}
+					c2 = 0;
+					break;
+				}
+				case 2: {
+					strcpy_s(p, "2");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					while (c2 != 3) {
+						strcpy_s(p, "Меню принятия решения:\n 1)Вывод таблицы незавершенных проектов;\n 2)Вывод завершенных непросмотренных проектов;\n 3)Выйти в меню администратора.\n");
+						send((SOCKET)newS, p, sizeof(p), 0);
+						recv((SOCKET)newS, m, sizeof(m), 0);
+						c2 = atoi(m);
+						switch (c2) {
+						case 1: {
+							strcpy_s(p, "1");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 2: {
+							strcpy_s(p, "2");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 3: {
+							strcpy_s(p, "3");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						}
+					}
+					c2 = 0;
+					break;
+				}
+				case 3: {
+					strcpy_s(p, "3");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					while (c2 != 5) {
+						strcpy_s(p, "Меню управления экспертами:\n 1)Добавление эксперта;\n 2)Вывод таблицы со всеми экспертами в алфавитном порядке;\n 3)Редактирование данных эксперта;\n 4)Удаление эксперта;\n 5)Выйти в меню администратора.\n");
+						send((SOCKET)newS, p, sizeof(p), 0);
+						recv((SOCKET)newS, m, sizeof(m), 0);
+						c2 = atoi(m);
+						switch (c2) {
+						case 1: {
+							strcpy_s(p, "1");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 2: {
+							strcpy_s(p, "2");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 3: {
+							strcpy_s(p, "3");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 4: {
+							strcpy_s(p, "4");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 5: {
+							strcpy_s(p, "5");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						}
+					}
+					c2 = 0;
+					break;
+				}
+				case 4: {
+					strcpy_s(p, "4");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					while (c2 != 4) {
+						strcpy_s(p, "Меню управления инвестиционными объектами:\n 1)Вывод таблицы со всеми свободными инвестиционными объектами в алфавитном порядке;\n 2)Редактирование данных инвестиционного объекта;\n 3)Удаление инвестиционного объекта;\n 4)Выйти в меню администратора.\n");
+						send((SOCKET)newS, p, sizeof(p), 0);
+						recv((SOCKET)newS, m, sizeof(m), 0);
+						c2 = atoi(m);
+						switch (c2) {
+						case 1: {
+							strcpy_s(p, "1");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 2: {
+							strcpy_s(p, "2");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 3: {
+							strcpy_s(p, "3");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						case 4: {
+							strcpy_s(p, "4");
+							send((SOCKET)newS, p, sizeof(p), 0);
+							break;
+						}
+						}
+					}
+					c2 = 0;
+					break;
+				}
+				case 5: {
+					strcpy_s(p, "5");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				}
+			}
+			c1 = 0;
+			break;
+		}
+		case 2: {
+			strcpy_s(p, "2");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			p[0] = '\0';
+			strcpy_s(p, "*ЗДЕСЬ БУДЕТ АВТОРИЗАЦИЯ*");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			while (c1 != 5) {
+				strcpy_s(k, "Меню клиента:\n 1)Редактирование данных;\n 2)Внос денежных средств;\n 3)Просмотр инвестиционного портфеля;\n 4)Разрыв договора с инвестиционной компанией;\n 5)Выйти в главное меню.\n ");
+				send((SOCKET)newS, k, sizeof(k), 0);
+				recv((SOCKET)newS, m, sizeof(m), 0);
+				c1 = atoi(m);
+				switch (c1) {
+				case 1: {
+					strcpy_s(p, "1");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				case 2: {
+					strcpy_s(p, "2");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				case 3: {
+					strcpy_s(p, "3");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				case 4: {
+					strcpy_s(p, "4");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				case 5: {
+					strcpy_s(p, "5");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				}
+			}
+			c1 = 0;
+			break;
+		}
+		case 3: {
+			strcpy_s(p, "3");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			p[0] = '\0';
+			strcpy_s(k, "*Здесь будет регистрация инвестиционного объекта его представителем*\n ");
+			send((SOCKET)newS, k, sizeof(k), 0);
+			break;
+		}
+		case 4: {
+			strcpy_s(p, "4");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			p[0] = '\0';
+			strcpy_s(p, "*ЗДЕСЬ БУДЕТ АВТОРИЗАЦИЯ*");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			while (c1 != 3) {
+				strcpy_s(k, "Меню эксперта:\n 1)Редактирование данных;\n 2)Выбор незавершенного проекта;\n 3)Выйти в главное меню.\n ");
+				send((SOCKET)newS, k, sizeof(k), 0);
+				recv((SOCKET)newS, m, sizeof(m), 0);
+				c1 = atoi(m);
+				switch (c1) {
+				case 1: {
+					strcpy_s(p, "1");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				case 2: {
+					strcpy_s(p, "2");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				case 3: {
+					strcpy_s(p, "3");
+					send((SOCKET)newS, p, sizeof(p), 0);
+					break;
+				}
+				}
+			}
+			c1 = 0;
+			break;
+		}
+		case 5: {
+			strcpy_s(p, "5");
+			send((SOCKET)newS, p, sizeof(p), 0);
+			closesocket((SOCKET)newS);
+			exit(EXIT_SUCCESS);
+			break;
+		}
+		}
+
+		///(c = recv((SOCKET)newS, p, sizeof(p), 0) != 0)
+	}
+
+}
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+	wVersionRequested = MAKEWORD(2, 2);
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0)
+		return -1;
+	sockaddr_in local;
+	local.sin_family = AF_INET;
+	local.sin_port = htons(1280);
+	local.sin_addr.s_addr = htonl(INADDR_ANY);
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+	int c = bind(s, (struct sockaddr*)&local, sizeof(local));
+	int r = listen(s, 5);
+	while (true) {
+		sockaddr_in remote;
+		int j = sizeof(remote);
+		SOCKET newS = accept(s, (struct sockaddr*)&remote, &j);
+		_beginthread(main_working, 0, (void*)newS);  //создание потока(Начальный адрес процедуры, который начинает выполнение нового потока; размер стека нового потока или 0; список аргументов, передаваемый в новый поток, или NULL)
+	}
+	WSACleanup();
+	return 0;
+}
